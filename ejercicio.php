a2507d6a2b19d34b796804a06d4d4ea29945951f
Hans Merz <erichhans10@gmail.com>
2017-11-21 07:56:10 -0500

    add img,and btn

--- ejercicio.php
+++ ejercicio.php
@@ -6,8 +6,12 @@
   </head>
   <body>
     <h1>Git lo mejor :v</h1>
+    <div class="img">
+      <img src="img.jpg" alt="">
+    </div>
     <form class="git" action="index.html" method="post">
       <input type="text" name="campo" value="">
+      <input type="submit" name="btn" value="">
     </form>
   </body>
 </html>
